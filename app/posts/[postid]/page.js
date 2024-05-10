52c8325f7a894b23ca00be4f4937d3a0ae8eb363
Aryan Rana <aryan.rana2005@gmail.com>
2024-05-10 13:31:16 +0530

    got comments from firestore and created comment component

--- app/posts/[postid]/page.js
+++ app/posts/[postid]/page.js
@@ -7,9 +7,16 @@ import { ModalState } from "../../context/ModalState";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
-import { onSnapshot, doc } from "firebase/firestore";
+import {
+  onSnapshot,
+  doc,
+  query,
+  collection,
+  orderBy,
+} from "firebase/firestore";
 import Post from "@/components/Post";
 import { db } from "@/firebase";
+import Comment from "@/components/Comment";
 
 export default function Postpage({ params }) {
   const postID = params.postid;
@@ -18,16 +25,28 @@ export default function Postpage({ params }) {
   const [open, setOpen] = useState(false);
   const [postId, setPostId] = useState("");
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
-  useEffect(()=>{
-    onSnapshot(doc(db, "posts", postID), (snapshot)=>{
+  // getting post data
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", postID), (snapshot) => {
       setPost(snapshot);
-    })
-  }, [db, postID])
+    });
+  }, [db, postID]);
 
-  // if (!session) {
-  //   router.push("/");
-  // }
+  // getting post comments
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", postID, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, postID]);
 
   return (
     <div>
@@ -37,12 +56,16 @@ export default function Postpage({ params }) {
 
           <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
             <div className="flex items-center py-2 px-3 sticky bg-white top-0 z-50 border-b border-gray-200 space-x-1">
-              <div className="hoverEffect" onClick={()=> router.push("/")}>
+              <div className="hoverEffect" onClick={() => router.push("/")}>
                 <ArrowLeftIcon className="h-5 font-bold" />
               </div>
               <h2 className="font-bold sm:text-xl text-lg">Home</h2>
             </div>
-            <Post post={post} id={postID}/>
+            <Post post={post} id={postID} />
+            {comments.length > 0 &&
+              comments.map((comment) => (
+                <Comment key={comment.id} id={comment.id} comment={comment.data()} />
+              ))}
           </div>
 
           <Widgets />
